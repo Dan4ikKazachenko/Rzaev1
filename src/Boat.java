9b8af4fd98f89af4bec8ee84d1af6383b5aec3f2
Dan4ikKazachenko <144421896+Dan4ikKazachenko@users.noreply.github.com>
2023-09-20 11:12:32 +0300

    Update Boat.java

--- src/Boat.java
+++ src/Boat.java
@@ -3,18 +3,20 @@ public class Boat {
     private double length;
     private int maxSpeed;
     private String color;
+
     public Boat(String name, double length, int maxSpeed, String color) {
         this.name = name;
         this.length = length;
         this.maxSpeed = maxSpeed;
         this.color = color;
     }
+
     public void startEngine() {
-        System.out.println(name + " завел двигатель.");
+        System.out.println(name + " started the engine.");
     }
 
     public void stopEngine() {
-        System.out.println(name + " остановил двигатель.");
+        System.out.println(name + " stopped the engine.");
     }
 
     public String getName() {
